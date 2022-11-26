4d7c8e502214eb601e001e29ebf3318a1f0bc8b8
JhojanD <jhojan1806.study@gmail.com>
2022-11-26 08:17:14 -0500

    first commit

---
+++ Works/Suma.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace Suma
+{
+   public class main_class
+   {
+      static System.Random random_generator = new System.Random();
+      public static void Main(string[] args)
+      {
+         string raptor_prompt_variable_zzyz;
+         ?? num2;
+         ?? num1;
+      
+         raptor_prompt_variable_zzyz ="Ingresa el Primer Número";
+         Console.WriteLine(raptor_prompt_variable_zzyz);
+         num1= Double.Parse(Console.ReadLine());
+         raptor_prompt_variable_zzyz ="Ingresa el segundo número";
+         Console.WriteLine(raptor_prompt_variable_zzyz);
+         num2= Double.Parse(Console.ReadLine());
+         Console.WriteLine("Resultado es: "+(num1+num2));
+      }
+   }
+}
